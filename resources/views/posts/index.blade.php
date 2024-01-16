503667c7ddb05c516454981068019683f711bd04
raimeiga <yusuke612@hotmail.com>
2024-01-16 16:47:32 +0900

    16.8 各ビューを編集してBootstrapのclassを追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -9,42 +9,42 @@
       @vite(['resources/js/app.js'])
  </head>
  
- <body>
+ <body style="padding: 60px 0;">
      <header>
-         <nav>
-             <div>
-                  <a href="{{ route('posts.index') }}">投稿アプリ</a>            
-             </div>
+        <nav class="navbar navbar-light bg-light fixed-top" style="height: 60px;">
+            <div class="container">  
+                  <a href="{{ route('posts.index') }}" class="navbar-brand">投稿アプリ</a>                     
+            </div>
          </nav>
      </header>
      
      <main>
          <article>
-             <div>                
-                 <h1>投稿一覧</h1>        
+             <div class="container">                  
+                 <h1 class="fs-2 my-3">投稿一覧</h1>        
                   @if (session('flash_message'))
-                     <p>{{ session('flash_message') }}</p>
-                 @endif
+                    <p class="text-success">{{ session('flash_message') }}</p>
+                @endif
  
-                 <div>
-                     <a href="{{ route('posts.create') }}">新規投稿</a>                                   
-                 </div>       
+                 <div class="mb-2">
+                     <a href="{{ route('posts.create') }}" class="text-decoration-none">新規投稿</a>                                   
+                 </div>      
 
                   @foreach($posts as $post)
-                     <div>
-                         <div>
-                             <h2>{{ $post->title }}</h2>
-                             <p>{{ $post->content }}</p>     
+                     <div class="card mb-3">
+                         <div class="card-body">
+                             <h2 class="card-title fs-5">{{ $post->title }}</h2>
+                             <p class="card-text">{{ $post->content }}</p>      
                              
-                              <div>
-                                 <a href="{{ route('posts.show', $post) }}">詳細</a>     
-                                 <a href="{{ route('posts.edit', $post) }}">編集</a>      
-
+                              <div class="d-flex">
+                                 <a href="{{ route('posts.show', $post) }}" class="btn btn-outline-primary d-block me-1">詳細</a>     
+                                 <a href="{{ route('posts.edit', $post) }}" class="btn btn-outline-primary d-block me-1">編集</a>  
+                                   
                                  <form action="{{ route('posts.destroy', $post) }}" method="post">
                                      @csrf
                                      @method('delete')                                        
-                                     <button type="submit">削除</button>
-                                 </form>
+                                     <button type="submit" class="btn btn-outline-danger">削除</button>
+                                </form>
 
                              </div>
                          </div>
@@ -55,9 +55,9 @@
          </article>
      </main>
  
-     <footer>        
-         <p>&copy; 投稿アプリ All rights reserved.</p>
-     </footer>
+     <footer class="d-flex justify-content-center align-items-center bg-light fixed-bottom" style="height: 60px;">        
+         <p class="text-muted small mb-0">&copy; 投稿アプリ All rights reserved.</p>
+    </footer>
  </body>
  
  </html>
